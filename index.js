cc7035f3d3556cc15a59359909a5a59410457219
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-06-20 12:28:40 -0400

    random numbers are generated

---
+++ index.js
@@ -0,0 +1,7 @@
+var number1;
+var number2;
+
+number1 = Math.floor((Math.random() * 10) + 1);
+number2 = Math.floor((Math.random() * 10) + 1);
+document.getElementById('number1').innerHTML = number1;
+document.getElementById('number2').innerHTML = number2;
